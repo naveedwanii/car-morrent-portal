1ed70987f6c9c1f9cf2bd15277168f039c6d9f14
Naveed Wani <navid.wani11@gmail.com@gmail.com>
2023-09-20 12:13:33 +0530

    Ignore errors

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -11,7 +11,7 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const handleNavigation = () => {
     const newLimit = (pageNumber + 1) * 10
     const newPathname = updateSearchParams("limit", `${newLimit}`);
-    router.push(newPathname)
+    // router.push(newPathname)
   }
   return (
     <div className="w-full flex-center gap-5 mt-10">
